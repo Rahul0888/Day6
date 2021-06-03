bdba4add5f11f382ae934c6dee9fb9abbedf585a
Manojpeethala <peethalamanoj@gmail.com>
2021-06-03 14:12:20 +0530

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            ArrayList al = new ArrayList();
+            Console.Write("Enter Your Employee ID:");
+            al.Add(Console.ReadLine());
+            Console.Write("Enter Your Employee Name :");
+            al.Add(Console.ReadLine());
+            Console.Write("Enter Travel Date: ");
+            al.Add(Console.ReadLine());
+            Console.WriteLine("Enter Travel Location: ");
+            al.Add(Console.ReadLine());
+
+            Console.ReadLine();
+        }
+    }
+
+}
+
+
+
+
